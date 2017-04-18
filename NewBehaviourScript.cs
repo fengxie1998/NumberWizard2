4a68dfd343fe71330f7ff62a6df7c791b164bf74
Feng Xie <boltz3r0@gmail.com>
2017-04-18 00:23:54 -0700

    Add files via upload

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	int max;
+	int min;
+	int guess; 
+
+	void Start (){
+		StartGame ();
+	}
+	void StartGame () {
+		max = 1000;
+		min = 1;
+		guess = 500;
+		max = max + 1;
+
+		print ("==========================================================");
+		print ("welcome");
+		print ("type any #");
+	
+		print ("highest pick is 1000"+ max);
+		print ("lowest pick is 1" + min);
+	
+		print ("number lower" +guess);
+		print ("up arrow for higher, down for lower, return for equal");
+	}
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow)){
+			min = guess;
+			NextGuess();
+		} else if (Input.GetKeyDown(KeyCode.DownArrow)){
+			max = guess;
+			NextGuess ();
+		} else if (Input.GetKeyDown(KeyCode.Return)){
+			print("Won");
+			StartGame ();
+		}
+	}
+	void NextGuess () {
+		guess = (max + min) / 2;
+		print ("Higher or Lower than" + guess);
+		print ("Up = higher, down = lower, return = equal");
+	}
+}
